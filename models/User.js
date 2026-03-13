f8fdce0e94da0bc06109923fa3bd9c68264a1e8e
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-03-14 01:16:22 +0530

    Login Sign Up Finish !

--- models/User.js
+++ models/User.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
 
-// const { default: mongoose } = require("mongoose");
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email:{
 
-// const userSchema= new mongoose.Schema({
-//     username:{
-//           type:String,
-//           required:true,
-//           unique:true
-//     },
-//     password:{
-//         type:String,
-//         required:true
+    type:String,
+    required:true,
+    unique:true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-//     }
-// })
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const bodyParser=require('body-parser')
 const bcrypt=require('bcryptjs')
 const mongoose=require('mongoose')
-const app=express();
+const User = require('./models/user');
+const { JsonWebTokenError } = require('jsonwebtoken');
+const app = express();
+const jwt=require('jsonwebtoken')
+
+
 app.use(bodyParser.json())
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/FeedbackDb')
 .then(()=>console.log("DbConnected"))
@@ -14,26 +20,66 @@ app.get('/',(req,res)=>
     res.send("Server IS runnig at 3000")
 });
 
+
+
 //CREATING RAGISTER API
-app.post('/register', async(req,res)=>
-{
-    const {username,password}=req.body;
-    let users=[];
-    // console.log("Ragistering succesfully");
-    // console.log(username,password);
+app.post('/register', async (req,res)=>{
+    const {username,email,password} = req.body;
 
-    // Check if user exists
-    const exisitingUser=users.find(u=>u.username===username);
-    if(exisitingUser) return res.status(404).json({message:"user already exist"})
+    // Check if user exists in MongoDB
+    const exisitingUser = await User.findOne({ username: email});
 
-    //hash the password
-    const  hasedPassword=await bcrypt.hash(password,10);
+    if(exisitingUser){
+        return res.status(400).json({message:"User already exist"});
+    }
 
+    // hash password
+    const hashedPassword = await bcrypt.hash(password,10);
 
-    // save the password
-    users.push({username,password:hasedPassword})
-    res.json({message:"User registered successfully"})
+    // create user
+    const newUser = new User({
+        email:email,
+        username: username,
+        password: hashedPassword
+    });
+
+    // save in MongoDB
+    await newUser.save();
+
+    res.status(200).json({message:"User registered successfully"});
 });
+
+
+   app.post('/login',async (req,res)=>
+{
+    const{email,password}=req.body;
+    const user= await User.findOne({email});
+    if(!user)
+    {
+        return res.status(400).json({
+            message:"User Not Found"
+        })
+    }
+
+    const isMatch=await bcrypt.compare(password,user.password)
+    if(!isMatch)
+    {
+        return res.status(400).json({
+            message:"Incorrect Password"
+        })
+    }
+
+    const token = jwt.sign(
+        {id:user._id},
+        "JHBFIUWBFIUWB",
+        {expiresIn:'1h'}
+    )
+
+    res.json({
+        message:"Login Succesfully",
+        token:token
+    })
+})
 
 app.listen(3000,()=>
 {
